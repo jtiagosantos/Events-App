20d614b832dee2e86ebf890e1a88c511ab0941e9
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-09-01 10:02:45 -0500

    refactor: add environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCizbhv0Mob8eoQbTC7cjgI0_FDA8skUrc",
-  authDomain: "events-app-704a6.firebaseapp.com",
-  projectId: "events-app-704a6",
-  storageBucket: "events-app-704a6.appspot.com",
-  messagingSenderId: "823158904242",
-  appId: "1:823158904242:web:4a9dfc035064caec4cc0ab"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
-
 export default initializeApp(firebaseConfig);
 export { getAuth, signInWithEmailAndPassword };
