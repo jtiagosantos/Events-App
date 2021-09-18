d23f9e666ef1f098012d9ad57710ce9cedaaff80
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-09-18 00:06:31 -0500

    feat: add user login state to browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import store from './store';
+import { store, persistor } from './store'
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -14,18 +15,20 @@ import EventDetails from './pages/EventDetails';
 export function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/my_events" component={EventsByUser} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/recover_password" component={PasswordRecovery} />
-          <Route exact path="/register_event" component={EventRegistration} />
-          <Route exact path="/event_details/:id" component={EventDetails} />
-          <Route exact path="/edit_event/:id" component={EventRegistration} />
-        </Switch>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/my_events" component={EventsByUser} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/recover_password" component={PasswordRecovery} />
+            <Route exact path="/register_event" component={EventRegistration} />
+            <Route exact path="/event_details/:id" component={EventDetails} />
+            <Route exact path="/edit_event/:id" component={EventRegistration} />
+          </Switch>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,15 @@
 import { createStore } from "redux";
 import userReducer from './userReducer';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(userReducer);
+const persistConfig = {
+  key: 'events-app',
+  storage
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, userReducer);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
